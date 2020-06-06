8b8966650f31843146b31f4e5c32993cb7eb4755
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-06-06 08:43:58 -0400

    cost + per unit conditional

--- cost.py
+++ cost.py
@@ -1,4 +1,7 @@
 # cost = price x units
-def calc_base_cost(price, units):
-   cost = price * units
+def calc_base_cost(category, price, units):
+   if category == 'per-unit':
+       cost = price * units
+   else:
+        cost = 1
    return cost
--- test_cost.py
+++ test_cost.py
@@ -2,6 +2,13 @@ from cost import *
 import pytest
 
 def test_simple_cost():
+    category = 'pre'
     price = 2
     units = 2
-    assert calc_base_cost(price, units) == 4
+    assert calc_base_cost(category, price, units) == 1
+
+def test_simple_cost_and_type():
+    category = 'per-unit'
+    price = 2
+    units = 2
+    assert calc_base_cost(category, price, units) == 4
