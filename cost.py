c24da0417f5dc30b4aea3e6a9a52bb041b2670a3
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-06-06 09:14:55 -0400

    updated per-unit conditional, add markdown conditional

--- cost.py
+++ cost.py
@@ -1,8 +1,12 @@
 # cost = price x units
 def calc_base_cost(category, price, units):
    if category == 'per-unit':
-       cost = price * int(units)
+       cost = price * round(units)
    elif category == 'by weight':
+       cost = price * units
+   elif category == 'markdown':
+       discount = .3
+       price = (price * (1 - discount))
        cost = price * units
    else:
         cost = 1
--- test_cost.py
+++ test_cost.py
@@ -19,8 +19,8 @@ def test_simple_cost_and_types():
     units = 1.7
     assert calc_base_cost(category, price, units) == 4
 
-def test_simple_cost_and_types():
-    category = 'by weight'
-    price = 2
-    units = 1.7
-    assert calc_base_cost(category, price, units) == 3.4
+def test_simple_cost_and_markdown():
+    category = 'markdown'
+    price = 1
+    units = 2
+    assert calc_base_cost(category, price, units) == 1.4
